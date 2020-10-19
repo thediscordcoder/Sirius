c393d99af2d8a8f88570e2ca16e2f1493ca018bf
Azlxy <62254752+Azlxy@users.noreply.github.com>
2020-10-19 17:29:38 +0100

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+	discord: {
+		status: '%clientUserDisplayName%\'s account.', // The status of the Discord client.
+		statusType: 'WATCHING', // The type of the Discord status.
+		token: 'TOKEN', // Discord token.
+		prefix: '>', // Discord prefix.
+		ownerIDs: ['OWNER_ID'], // The people that can use your bot.
+		ownerOnly: true // Recommended.
+	},
+
+	fortnite: {
+		cid: 'CID_028_Athena_Commando_F', // Default skin.
+		bid: 'BID_004_BlackKnight', // Default backpack.
+		eid:'EID_Floss', // Default emote.
+		pickaxe_id: 'Pickaxe_LockJaw', // Default pickaxe.
+		
+		status: 'Hey!', // Custom status.
+		platform: 'PSN', // Platform.
+
+		acceptFriend: true, // Toggle friend requests.
+		acceptInvite: true, // Toggle party invites.
+		joinMessage: 'Hey %memberName%!', // Message to send if someone joins.
+	}
+}
