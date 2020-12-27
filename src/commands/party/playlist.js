38bafda1a1c028ca107a130f6abb17062d8f1662
Azlxy <62254752+Azlxy@users.noreply.github.com>
2020-12-27 18:21:46 +0000

    Update playlist.js

--- src/commands/party/playlist.js
+++ src/commands/party/playlist.js
@@ -13,7 +13,7 @@ module.exports = {
     const newPlaylist = args[0];
     if (!newPlaylist) return error2('Please provide a playlist.', message);
 
-    bot.party.setPlaylist(playlist).catch(err => { return error2('An error occured while trying to change the playlist.', message);});
-    success3(`Changed the playlist to ${playlist}.`, message);
+    bot.party.setPlaylist(newPlaylist).catch(err => { return error2('An error occured while trying to change the playlist.', message);});
+    success3(`Changed the playlist to ${newPlaylist}.`, message);
   }
 }
